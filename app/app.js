5fda29179395f986de9ce747ee1bd122fd04b42a
Wang Xing <wangstar1031@hotmail.com>
2019-09-02 06:38:07 +0800

    Some changed on initDevice

--- app/app.js
+++ app/app.js
@@ -9,59 +9,9 @@ var input;
 var interval;
 //MediaStreamAudioSourceNode we'll be recording 
 // shim for AudioContext when it's not avb. 
-var AudioContext;
-var audioContext;
-window.onload = function() {
-	return;
-	AudioContext = window.AudioContext || window.webkitAudioContext;
-	audioContext = new AudioContext();
-  // var context = new AudioContext();
-  // Setup all nodes
-  // ...
+var AudioContext = null;
+var audioContext = null;
 
-	/* Simple constraints object, for more advanced audio features see
-
-	https://addpipe.com/blog/audio-constraints-getusermedia/ */
-
-	var constraints = {
-	    audio: true,
-	    video: false
-	} 
-	/* Disable the record button until we get a success or fail from getUserMedia() */
-
-	recordButton.disabled = true;
-	stopButton.disabled = false;
-	pauseButton.disabled = false
-
-	/* We're using the standard promise based getUserMedia()
-
-	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia */
-
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-	    console.log("getUserMedia() success, stream created, initializing Recorder.js ..."); 
-	    /* assign to gumStream for later use */
-	    gumStream = stream;
-	    /* use the stream */
-	    input = audioContext.createMediaStreamSource(stream);
-	    /* Create the Recorder object and configure to record mono sound (1 channel) Recording 2 channels will double the file size */
-	    rec = new Recorder(input, {
-	        numChannels: 1
-	    }) 
-	    //start the recording process 
-	    rec.record()
-	    console.log("Recording started");
-	    interval = setInterval(function(){
-	    	recordingFunction();
-	    }, 1000);
-	}).catch(function(err) {
-	    //enable the record button if getUserMedia() fails 
-	    console.log( err);
-	    recordButton.disabled = false;
-	    stopButton.disabled = true;
-	    pauseButton.disabled = true
-	});
-
-}
 function recordingFunction(){
 	// console.log("recordingFunction");
 	rec.exportWAV(function(_blob){
@@ -83,6 +33,10 @@ stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
 
 function initDevice(){
+	if( AudioContext && audioContext){
+		alert("Audio Context already initialized.");
+		return;
+	}
 	AudioContext = window.AudioContext || window.webkitAudioContext;
 	audioContext = new AudioContext();
   // var context = new AudioContext();
